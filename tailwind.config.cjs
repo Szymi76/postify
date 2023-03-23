f1983bc58ad814731da52f37f3ae2dc3def89590
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-03-23 14:09:35 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#3b82f6",
+          "secondary": "#ffffff",
+          "accent": "#1d4ed8",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      }
+    ]
+  }
+};
+
+module.exports = config;
